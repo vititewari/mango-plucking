7f5ecc1cf7fd9efa803b1e8274f156a507bd48b2
vititewari <70894215+vititewari@users.noreply.github.com>
2020-10-22 17:11:41 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -8,7 +8,7 @@ class tree
 		this.dustbinHeight=600;
 		this.wallThickness=10;
 		
-		this.image=loadImage("images/tree.png")
+		this.image=loadImage("tree.png")
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(0, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
 		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false})
